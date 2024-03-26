8bbb6aedf6b85de4697c79fa46574c8b434d6c7d
ikerikks <ikerikks@gmail.com>
2024-03-26 23:27:45 +0100

    creation of a private field

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,27 @@
+bool isNum(String char)=> num.tryParse(char) is num;
+bool isUpper(String char)=> char == char.toUpperCase() && !isNum(char);
+bool isLower(String char)=> char == char.toLowerCase()&& !isNum(char);
+
+class Password {
+  String _password = '';
+  bool isValid() {
+    int upper = 0;
+    int lower = 0;
+    int digits = 0;
+    int l = _password.length;
+
+    if (l < 8 || l >16) { return false; }
+    for (String char in _password.split('')) {
+      if (upper > 0 && lower > 0 && digits > 0) { return true; }
+      if (isNum(char)) { digits += 1; }
+      else if (isUpper(char)) { upper += 1; }
+      else if (isLower(char)) { lower += 1; }
+    }
+    return false;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
