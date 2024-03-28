cfbf9ea1ebb7c12de02679837a60450a56370204
ikerikks <ikerikks@gmail.com>
2024-03-28 17:32:20 +0100

    inheritance task

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,35 @@
+import '6-password.dart';
+
+class User extends Password{
+  String name = '';
+  int age = 0;
+  double height = 0;
+  int id = 0;
+  String user_password = '';
+
+  User({name, age, height, id, user_password}){
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.id = id;
+    this.user_password= user_password;
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+    id: userJson['id'],
+    name: userJson['name'],
+    age: userJson['age'],
+    height: userJson['height'],
+    user_password: userJson['user_password']
+    );
+  }
+
+  toJson()=> {'id': this.id, 'name': this.name, 'age': this.age, 'height': this.height};
+  @override
+  String toString() {
+    super.password = user_password;
+    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}), Password: ${super.isValid()}';
+  }
+
+}
---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,34 @@
+bool isNum(String char)=> num.tryParse(char) is num;
+bool isUpper(String char)=> char == char.toUpperCase() && !isNum(char);
+bool isLower(String char)=> char == char.toLowerCase()&& !isNum(char);
+
+class Password{
+  String _password = '';
+  Password({password = ''}) {
+    _password = password;
+  }
+  
+  String get password => _password;
+  void set password(String value) {
+    _password = value;
+  }
+
+  bool isValid() {
+    int upper = 0;
+    int lower = 0;
+    int digits = 0;
+    int l = _password.length;
+
+    if (l < 8 || l > 16) { return false; }
+    for (String char in _password.split('')) {
+      if (isNum(char)) { digits += 1; }
+      else if (isUpper(char)) { upper += 1; }
+      else if (isLower(char)) { lower += 1; }
+      if (upper > 0 && lower > 0 && digits > 0) { return true; }
+    }
+    return false;
+  }
+
+  @override
+  String toString() => "Your Password is: ${_password}";
+}
