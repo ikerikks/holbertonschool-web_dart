22b83a34b92450952fb235e412c7b114b09034ef
ikerikks <ikerikks@gmail.com>
2024-03-28 11:42:04 +0100

    json to User: code update

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,20 +1,24 @@
 class User {
   Map map = {};
+  String name = '';
+  int age = 0;
+  double height = 0;
 
   User({name, age, height, id}){
-    this.map.addAll({
-    'id': id,
-    'name': name,
-    'age': age,
-    'height': height
-    });
+    this.name = name;
+    this.age = age;
+    this.height = height;
   }
 
-  User.fromJson(map) {
-    this.map.addAll(map);
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+    id: userJson['id'],
+    name: userJson['name'],
+    age: userJson['age'],
+    height: userJson['height'],
+  );
   }
 
-  toJson()=> map;
-  @override
-  String toString()=> 'User(id : ${map['id']}, name: ${map['name']}, age: ${map['age']}, height: ${map['height']})';
+  toJson()=> {'name': name, 'age': age, 'height': height};
 }
+
