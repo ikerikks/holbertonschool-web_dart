674855df913263483f7baca65c9527afc2c637c8
ikerikks <ikerikks@gmail.com>
2024-03-30 17:19:10 +0100

    function update

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,21 +1,21 @@
-int totalPoints(Map team) {
-  num result = 0;
+int totalPoints(Map<String, int> team) {
+  int result = 0;
   
   team.forEach((key, value) {
     switch(key) {
       case '3 pointers':
-        result += (3 * team[key]);
+        result += (3 * value);
         break;
       
       case '2 pointers':
-        result += (2 * team[key]);
+        result += (2 * value);
         break;
 
       default:
-        result += (1 * team[key]);
+        result += (1 * value);
     }
   });
-  return result.toInt();
+  return result;
 }
 
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
