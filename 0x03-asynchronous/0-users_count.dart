2e4308c2095d0b49e33352f458f4e8d69580980a
ikerikks <ikerikks@gmail.com>
2024-04-02 11:21:11 +0200

    update

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,10 +1,13 @@
 import './util.dart';
 
-Future<void> usersCount() async {
-  var result = await fetchUsersCount();
-  return Future<void>.delayed(Duration(seconds: 0), () {
-   print(result);
-  });
+Future<void> usersCount(){
+  // var result = fetchUsersCount();
+  // return Future.delayed(Duration(seconds: 0), () {
+  //  print(result);
+  // });
+ 
+  return fetchUsersCount()
+    .then((value) { print(value); });
   // return (fetchUsersCount()
   // .then((value) { print(value); })
   // .catchError((e) {
