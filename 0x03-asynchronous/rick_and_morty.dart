008affb275046c30aa798b6971ecb21bf65fcbf0
ikerikks <ikerikks@gmail.com>
2024-04-03 16:39:15 +0200

    Displaying Ricky & Morty characters getted from an api

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<dynamic> printRmCharacters() async {
+  try {
+    var url = Uri.parse('https://rickandmortyapi.com/api/character');
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+    var characters = data['results'];
+
+    characters.forEach((character) {
+      print(character['name']);
+    });
+  } catch(e) {
+    return 'error caughy: $e';
+  }
+}
