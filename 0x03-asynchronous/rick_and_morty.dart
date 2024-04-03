182c58cc51d53215b1b13c4bc1faa4343a5bf0b6
ikerikks <ikerikks@gmail.com>
2024-04-03 23:27:25 +0200

    checking status of fetching

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -5,12 +5,14 @@ Future<dynamic> printRmCharacters() async {
   try {
     var url = Uri.parse('https://rickandmortyapi.com/api/character');
     var response = await http.get(url);
-    var data = jsonDecode(response.body);
-    var characters = data['results'];
-
-    characters.forEach((character) {
-      print(character['name']);
-    });
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var characters = data['results'];
+  
+      characters.forEach((character) {
+        print(character['name']);
+      });
+    }
   } catch(e) {
     return 'error caught: $e';
   }
